e78c65ccbc250591b2bd3fda95c87276ccc66ae0
Tomas Ipperi <tipperi@bdev.com.ar>
2026-03-11 16:19:56 -0300

    Favicon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+
+  app: {
+    head: {
+      title: 'Sitio Oficial - Atlético Sur Gualeguaychú',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/logo.png' }
+      ],
+    },
+  },
+
   nitro: {
     preset: 'static'
   },
